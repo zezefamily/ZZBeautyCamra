0e5a5278f6615647ce3b81d2f0980a2956df0337
泽泽 <zezefamily@163.com>
2020-08-18 18:56:18 +0800

    新增StoryBoard 内容

---
+++ ZZBeautyCamra/ZZBeautyCamra/pages/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  ZZBeautyCamra
+//
+//  Created by 泽泽 on 2020/8/18.
+//  Copyright © 2020 泽泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZZBeautyCamra/ZZBeautyCamra/pages/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  ZZBeautyCamra
+//
+//  Created by 泽泽 on 2020/8/18.
+//  Copyright © 2020 泽泽. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
